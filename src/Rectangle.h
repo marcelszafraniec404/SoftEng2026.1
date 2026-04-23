d381b91dcffe2a75667803c0fe790e2cdfc3c5c2
Eryk Nowacki <eryknowacki1@gmail.com>
2026-04-23 21:44:52 +0200

    129730 oraz 129693 (#26)
    * Zmieniam to żeby sprawdzić czy działa
    
    * 129730 oraz 129693 zainplementowalismy zmiany w rectangle
    
    * rozwiązanie konfliktu
    
    * Poprawiliśmy zadanie. Mamy nadzieje że poprawnie
    
    * Naprawione :)
    
    * Teraz u mnie kompiluje się normalnie (sprawdziłem 5 razy)
    
    ---------
    
    Co-authored-by: Eryk Nowacki <129730@uksw.edu.pl>

--- src/Rectangle.h
+++ src/Rectangle.h
@@ -1,35 +1,44 @@
 #ifndef _RECTANGLE_H
 #define _RECTANGLE_H
 
-
 #include "Shape2D.h"
 #include "ShapeResultData.h"
 #include <string>
 using namespace std;
 #include "ShapeParam.h"
 
-template<class T>
-class Rectangle : public Shape2D<T> {
-  public:
+template <class T> class Rectangle : public Shape2D<T> {
+public:
     inline virtual ShapeResult<T> compute();
-
     inline string print();
-
-    inline Rectangle(const ShapeParam<T> & param);
-
+    inline Rectangle(const ShapeParam<T>& param);
 };
-template<class T>
-inline ShapeResult<T> Rectangle<T>::compute() {
-  return ShapeResult<T>();
+
+template <class T> inline ShapeResult<T> Rectangle<T>::compute()
+{
+    T a = this->m_param.get_attrib(PARAM_WIDTH);
+    T b = this->m_param.get_attrib(PARAM_HEIGHT);
+
+    T area = a * b;
+    T perimeter = 2 * (a + b);
+
+    ShapeResult<T> res;
+    res.set_attrib(RESULT_AREA, area);
+    res.set_attrib(RESULT_PERIMETER, perimeter);
+
+    return res;
 }
 
-template<class T>
-inline string Rectangle<T>::print() {
-  return "";
+template <class T> inline string Rectangle<T>::print()
+{
+    T a = this->m_param.get_attrib(PARAM_WIDTH);
+    T b = this->m_param.get_attrib(PARAM_HEIGHT);
+
+    return "Rectangle a=" + to_string(a) + " b=" + to_string(b);
 }
 
-template<class T>
-inline Rectangle<T>::Rectangle(const ShapeParam<T> & param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Rectangle<T>::Rectangle(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
 #endif
