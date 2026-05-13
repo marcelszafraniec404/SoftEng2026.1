c507e03009321f84365aee73470708627c745632
Szafraniec <marcel.szafraniec@gmail.com>
2026-05-13 11:34:22 +0200

    poprawa

--- src/ShapeParam.h
+++ src/ShapeParam.h
@@ -36,17 +36,25 @@ inline bool ShapeParam<T>::set_attrib(ShapeParamIndex ind, const T & val) {
   return true;
 }
 
-template <class T> inline bool ShapeParam<T>::validate() const
+template<class T>
+inline bool ShapeParam<T>::validate() const
 {
-    switch (type)
+    switch(type)
     {
-        case ShapeType::PT_HEXAGON: {
-         T radius = get_attrib(PARAM_RADIUS);
-         if (radius < 0) return false;
-         break;
+        case ShapeType::PT_HEXAGON:
+        {
+            T radius = get_attrib(PARAM_RADIUS);
+
+            if (radius < 0)
+                return false;
+
+            break;
         }
-        default: break;
+
+        default:
+            break;
     }
+
     return true;
 }
 
